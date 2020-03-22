337f8ad84413cede56a8df89ef93f125a63442cd
Henry Turner <harry.turner@rovco.com>
2020-03-22 15:37:50 +0000

    Removed slashes from routes and made them non exact.

--- src/App.js
+++ src/App.js
@@ -41,7 +41,7 @@ class App extends React.Component {
                     <Route exact path="/">
                         <LandingPage user={this.state.user}/>
                     </Route>
-                    <Route exact path="/login/">
+                    <Route path="/login">
                         <LoginPage />
                     </Route>
                     <Route path="/portal/">
@@ -82,7 +82,7 @@ class AuthorisedArea extends React.Component {
                     </Navbar.Collapse>
                 </Navbar>
                 <Switch>
-                    <Route exact path="/portal/">
+                    <Route path="/portal">
                         <Dashboard />
                     </Route>
                 </Switch>
